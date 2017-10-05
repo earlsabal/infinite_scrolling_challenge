f7c0474adda8efbe6cc91fb469b3a7819b9af17a
earlsabal <earlsabal@gmail.com>
2017-10-05 10:27:51 -0700

    Update controller to take in JSON file, generate pins, and paginate them

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,33 @@
 class PinsController < ApplicationController
 
+	ALL_PINS = JSON.parse(File.read("pins_formatted.json"))
+	START_PAGE = 1
+	PER_PAGE = 5
+
 	def index
+		@file = File.read("pins_formatted.json")
+		@pins = JSON.parse(@file).paginate(page: params[:page], per_page: 5)
+	end
+
+	
+
+	def generate_pins(page)
+
+		current_pins = ALL_PINS.paginate(page: page, per_page: PER_PAGE)
+		if current_pins.next_page == nil
+			current_pins = reset_pins(current_pins)
+		end
+		current_pins
 
 	end
+
+	def reset_pins(ending_pins)
+
+		resetted_pins = ending_pins + ALL_PINS
+		per_page = ending_pins.size + PER_PAGE
+		resetted_pins.paginate(page: START_PAGE, per_page: per_page)
+
+	end
+
 
 end
