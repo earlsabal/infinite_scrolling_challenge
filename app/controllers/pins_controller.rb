5540fe7284c6401bfb937fbf0915c6364bed32f0
earlsabal <earlsabal@gmail.com>
2017-10-05 10:42:00 -0700

    Refactor code out of controller into module

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,31 +1,10 @@
 class PinsController < ApplicationController
 
-	ALL_PINS = JSON.parse(File.read("pins_formatted.json"))
-	START_PAGE = 1
-	PER_PAGE = 5
+	include PinsHelper
 
 	def index
 
 		@pins = generate_pins(params[:page])
-
-	end
-
-	private
-	def generate_pins(page)
-
-		current_pins = ALL_PINS.paginate(page: page, per_page: PER_PAGE)
-		if current_pins.next_page == nil
-			current_pins = reset_pins(current_pins)
-		end
-		current_pins
-
-	end
-
-	def reset_pins(ending_pins)
-
-		resetted_pins = ending_pins + ALL_PINS
-		per_page = ending_pins.size + PER_PAGE
-		resetted_pins.paginate(page: START_PAGE, per_page: per_page)
 
 	end
 
---
+++ app/helpers/pins_helper.rb
@@ -0,0 +1,25 @@
+module PinsHelper
+
+	ALL_PINS = JSON.parse(File.read("pins_formatted.json"))
+	START_PAGE = 1
+	PER_PAGE = 5
+
+	def generate_pins(page)
+
+		current_pins = ALL_PINS.paginate(page: page, per_page: PER_PAGE)
+		if current_pins.next_page == nil
+			current_pins = reset_pins(current_pins)
+		end
+		current_pins
+
+	end
+
+	def reset_pins(ending_pins)
+
+		resetted_pins = ending_pins + ALL_PINS
+		per_page = ending_pins.size + PER_PAGE
+		resetted_pins.paginate(page: START_PAGE, per_page: per_page)
+
+	end
+
+end
