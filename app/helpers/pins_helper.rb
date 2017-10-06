975163e50a8071e0a2f23684023146ecd5457ad8
earlsabal <earlsabal@gmail.com>
2017-10-05 17:07:25 -0700

    Clean up code

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -6,16 +6,21 @@ module PinsHelper
 	START_PAGE = 1
 	PER_PAGE = 5
 
+	# Used to generate the pins with an integer param for page number
 	def generate_pins(page)
 
 		current_pins = ALL_PINS.paginate(page: page, per_page: PER_PAGE)
+
 		if current_pins.next_page == nil
 			current_pins = reset_pins(current_pins)
 		end
+
 		current_pins
 
 	end
 
+	# Resets list to beginning once last page is reached
+	# Will load remaining pins + the first 5 pins once this happens
 	def reset_pins(ending_pins)
 
 		resetted_pins = ending_pins + ALL_PINS
@@ -27,23 +32,33 @@ module PinsHelper
 	# VIEWS
 
 	def pin_image(pin)	
+
 		image_tag(pin["images"]["236x"]["url"])
+
 	end
 
 	def pin_description(pin)
+
 		pin["description"]
+
 	end
 
 	def pinner_username(pin)
+
 		pin["pinner"]["username"]
+
 	end
 
 	def pinner_pic(pin)
-		image_tag( pin["pinner"]["image_small_url"] )
+
+		image_tag(pin["pinner"]["image_small_url"])
+
 	end
 
 	def pin_created_at(pin)
+
 		pin["created_at"]
+
 	end
 
 end
